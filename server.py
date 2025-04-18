63acfd0af6e6753814b631a1c2adcd97af6453e1
Fawaz Alhafiz <fawaztwix111@gmail.com>
2025-04-18 23:24:22 +0200

    Starting tthe project

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # returning string
+    # return "Hello, World!, this is a Flask app running in a virtual environment!"
+
+    return {"message": "Hello, World!, this is a Flask app running in a virtual environment!"}
